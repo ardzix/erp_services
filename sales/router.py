05b5ab989ace2526cd160b05457a01c6f36d3a13
TM <ardzyix@gmail.com>
2023-09-26 17:51:26 +0700

    fix: routing name

--- sales/router.py
+++ sales/router.py
@@ -8,11 +8,11 @@ from .views.canvasing import (
 )
 
 router = DefaultRouter()
-router.register('canvassing_customer_visit',
-                CustomerVisitStatusUpdateViewSet, basename='canvassing_customer_visit')
-router.register('canvassing_trip_template',
-                TripTemplateViewSet, basename='canvassing_trip_template')
-router.register('canvassing_trip', TripViewSet,
-                basename='canvassing_trip')
+router.register('customer_visit',
+                CustomerVisitStatusUpdateViewSet, basename='customer_visit')
+router.register('trip_template',
+                TripTemplateViewSet, basename='trip_template')
+router.register('trip', TripViewSet,
+                basename='trip')
 router.register('customer', CustomerViewSet, basename='customer')
 router.register('sales_order', SalesOrderViewSet, basename='sales_order')
