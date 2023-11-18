4c981b160b5881d7f9a9c0f4af6e76bce8290d89
TM <ardzyix@gmail.com>
2023-11-18 11:44:06 +0700

    feat: add description to Configuration model

--- common/models/__init__.py
+++ common/models/__init__.py
@@ -75,6 +75,7 @@ class AdministrativeLvl4(AdministrativeBaseModel):
 class Configuration(models.Model):
     key = models.CharField(max_length=255, unique=True, help_text="The name of the configuration setting")
     value = models.TextField(help_text="The value of the configuration setting")
+    description = models.TextField(blank=True, null=True, help_text="The description of the configuration setting")
 
     def __str__(self):
         return f"{self.key}: {self.value}"
