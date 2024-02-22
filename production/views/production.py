9ed31a37478aa55869e6d04004860036813b0d27
Arif Dzikrullah <ardzyix@gmail.com>
2024-02-22 07:21:25 +0700

    fix: add pagination to production order

--- production/views/production.py
+++ production/views/production.py
@@ -1,14 +1,31 @@
-from rest_framework import viewsets
+from django_filters import rest_framework as django_filters
+from rest_framework import viewsets, filters
+from libs.pagination import CustomPagination
+from libs.filter import CreatedAtFilterMixin
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 from ..models import ProductionOrder, WorkOrder, ProductionTracking
 from ..serializers.production import ProductionOrderSerializer, WorkOrderSerializer, ProductionTrackingSerializer
 
 class ProductionOrderViewSet(viewsets.ModelViewSet):
     queryset = ProductionOrder.objects.all()
     serializer_class = ProductionOrderSerializer
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
+    pagination_class = CustomPagination 
+    filterset_class = CreatedAtFilterMixin
+    filter_backends = (django_filters.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ['product__name']
+    lookup_field = 'id32'
 
 class WorkOrderViewSet(viewsets.ModelViewSet):
     queryset = WorkOrder.objects.all()
     serializer_class = WorkOrderSerializer
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
+    pagination_class = CustomPagination 
+    filterset_class = CreatedAtFilterMixin
+    filter_backends = (django_filters.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ['production_order__product__name']
+    lookup_field = 'id32'
+
 
     def get_queryset(self):
         queryset = super().get_queryset()
