982119a02d97097d4c83b948cf1cf360401f9048
Arif Dzikrullah <ardzyix@gmail.com>
2024-02-29 16:04:08 +0700

    fix: database engine

--- erp_backoffice/default_local_settings.py
+++ erp_backoffice/default_local_settings.py
@@ -16,7 +16,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
