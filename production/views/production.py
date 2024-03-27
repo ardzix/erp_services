2f4f8476ae126bdcd4e36d0568d085d725963743
Arif Dzikrullah <ardzyix@gmail.com>
2024-03-27 20:59:21 +0700

    feat: ProductionTrackingFilter

--- production/views/production.py
+++ production/views/production.py
@@ -1,4 +1,5 @@
 from django_filters import rest_framework as django_filters
+from django.utils.translation import gettext_lazy as _
 from rest_framework import viewsets, filters
 from libs.pagination import CustomPagination
 from libs.filter import CreatedAtFilterMixin
@@ -37,13 +38,42 @@ class WorkOrderViewSet(viewsets.ModelViewSet):
         elif status_filter == 'finished':
             queryset = queryset.filter(end_time__isnull=False)
         return queryset
+    
+
+
+class ProductionTrackingFilter(django_filters.FilterSet):
+    start_time_range = django_filters.CharFilter(method='filter_start_time_range', help_text=_('Put date range in this format: start_date,end_date [YYYY-MM-DD,YYYY-MM-DD]'))
+    end_time_range = django_filters.CharFilter(method='filter_end_time_range', help_text=_('Put date range in this format: start_date,end_date [YYYY-MM-DD,YYYY-MM-DD]'))
+    work_center_warehouse_id32 = django_filters.CharFilter(field_name='work_center_warehouse__id32', lookup_expr='exact')
+
+    def filter_start_time_range(self, queryset, name, value):
+        if value:
+            # Split the value on a comma to extract the start and end dates
+            dates = value.split(',')
+            if len(dates) == 2:
+                start_date, end_date = dates
+                return queryset.filter(start_time__gte=start_date, start_time__lte=end_date)
+        return queryset
+
+    def filter_end_time_range(self, queryset, name, value):
+        if value:
+            # Split the value on a comma to extract the start and end dates
+            dates = value.split(',')
+            if len(dates) == 2:
+                start_date, end_date = dates
+                return queryset.filter(end_time__gte=start_date, end_time__lte=end_date)
+        return queryset
+    class Meta:
+        model = ProductionTracking
+        fields = ['start_time_range', 'end_time_range', 'work_center_warehouse_id32']
+
 
 class ProductionTrackingViewSet(viewsets.ModelViewSet):
     queryset = ProductionTracking.objects.all()
     serializer_class = ProductionTrackingSerializer
     permission_classes = [IsAuthenticated, DjangoModelPermissions]
     pagination_class = CustomPagination 
-    filterset_class = CreatedAtFilterMixin
+    filterset_class = ProductionTrackingFilter
     filter_backends = (django_filters.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
     search_fields = ['work_center_warehouse__name']
     lookup_field = 'id32'
