71491a3860681f333fd864181662eabc9a63afac
Fizi Kurniawan <fizikurniawan@gmail.com>
2024-05-02 20:39:00 +0700

    add export so and customer

--- sales/views/customer.py
+++ sales/views/customer.py
@@ -1,5 +1,8 @@
+import datetime
 from django.contrib.gis.db.models import Avg
 from django.db import connection
+from django.db.models import F, Sum
+from django.shortcuts import HttpResponse
 from django_filters import rest_framework as django_filters
 from django.utils.translation import gettext_lazy as _
 from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
@@ -8,13 +11,15 @@ from rest_framework.response import Response
 from rest_framework import viewsets, filters
 from libs.pagination import CustomPagination
 from libs.filter import CreatedAtFilterMixin
+from libs.excel import create_xlsx_file
 from ..serializers.customer import CustomerSerializer, CustomerListSerializer, CustomerMapSerializer, StoreTypeSerializer, CustomerCreateSerializer
 from ..models import Customer, StoreType, OrderItem
 
 
 class CustomerFilter(CreatedAtFilterMixin):
     id32s = django_filters.CharFilter(method='filter_by_id32s')
-    order_created_at_range = django_filters.CharFilter(method='filter_order_created_at_range', help_text=_('Put date range in this format: start_date,end_date [YYYY-MM-DD,YYYY-MM-DD]'))
+    order_created_at_range = django_filters.CharFilter(method='filter_order_created_at_range', help_text=_(
+        'Put date range in this format: start_date,end_date [YYYY-MM-DD,YYYY-MM-DD]'))
 
     def filter_order_created_at_range(self, queryset, name, value):
         return queryset
@@ -24,8 +29,9 @@ class StoreTypeViewSet(viewsets.ModelViewSet):
     lookup_field = 'id32'
     queryset = StoreType.objects.all()
     permission_classes = [IsAuthenticated, DjangoModelPermissions]
-    pagination_class = CustomPagination 
+    pagination_class = CustomPagination
     serializer_class = StoreTypeSerializer
+
 
 class CustomerViewSet(viewsets.ModelViewSet):
     """
@@ -53,39 +59,40 @@ class CustomerViewSet(viewsets.ModelViewSet):
     lookup_field = 'id32'
     queryset = Customer.objects.all()
     permission_classes = [IsAuthenticated, DjangoModelPermissions]
-    pagination_class = CustomPagination 
+    pagination_class = CustomPagination
     filter_backends = (django_filters.DjangoFilterBackend,
                        filters.OrderingFilter)
     filterset_class = CustomerFilter
 
     def get_serializer_class(self):
-        if self.action == 'list':
-            return CustomerListSerializer
-        elif self.action == 'map':
-            return CustomerMapSerializer
-        elif self.action == 'create':
-            return CustomerCreateSerializer
-        return CustomerSerializer
-    
+        return {
+            "list": CustomerListSerializer,
+            "map": CustomerMapSerializer,
+            "create": CustomerCreateSerializer,
+            "excel": None
+        }.get(self.action, CustomerSerializer)
+
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
-        order_created_at_range = request.query_params.get("order_created_at_range")
+        order_created_at_range = request.query_params.get(
+            "order_created_at_range")
         order_items = OrderItem.objects.filter(order__customer__in=queryset)
         if order_created_at_range:
             order_created_at_range = order_created_at_range.split(',')
             if len(order_created_at_range) == 2:
                 start_date, end_date = order_created_at_range
-                order_items = order_items.filter(order__created_at__gte=start_date, order__created_at__lte=end_date)
-        
+                order_items = order_items.filter(
+                    order__created_at__gte=start_date, order__created_at__lte=end_date)
+
         context = {"order_items": order_items}
 
         page = self.paginate_queryset(queryset)
         if page:
             serializer = self.get_serializer(page, many=True, context=context)
-            return Response(serializer.data)
+            return self.get_paginated_response(serializer.data)
         else:
             serializer = self.get_serializer(queryset, context=context)
-            return self.get_paginated_response(serializer.data)
+            return Response(serializer.data)
 
     def create(self, requests, *args, **kwargs):
         return super().create(requests, *args, **kwargs)
@@ -102,7 +109,7 @@ class CustomerViewSet(viewsets.ModelViewSet):
             AVG(ST_X(location::geometry)) as avg_longitude 
         FROM sales_customer
         """
-        
+
         # Execute the raw SQL
         with connection.cursor() as cursor:
             cursor.execute(raw_sql)
@@ -119,3 +126,65 @@ class CustomerViewSet(viewsets.ModelViewSet):
             },
             "markers": serializer.data
         })
+
+    @action(methods=["GET"], detail=False, url_path="excel")
+    def export_excel(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+        order_created_at_range = request.query_params.get(
+            "order_created_at_range")
+        order_items = OrderItem.objects.filter(order__customer__in=queryset)
+        if order_created_at_range:
+            order_created_at_range = order_created_at_range.split(',')
+            if len(order_created_at_range) == 2:
+                start_date, end_date = order_created_at_range
+                order_items = order_items.filter(
+                    order__created_at__gte=start_date, order__created_at__lte=end_date)
+
+        headers = {
+            "customer": "Customer",
+            "omzet": "Omzet",
+            "qty": "Quantity",
+            "margin": "Margin",
+            "margin_percent": "% Margin"
+        }
+
+        def get_item(customer):
+            current_order_items = order_items.filter(order__customer=customer)
+            aggragators = current_order_items.aggregate(total_omzet=Sum(F('price') * F('quantity')), total_qty=Sum('quantity'),
+                                                        total_margin=F('total_omzet') - Sum(F('product__base_price') * F('quantity')))
+            total_omzet = aggragators.get("total_omzet") or 0
+            total_qty = aggragators.get("total_qty") or 0
+            total_margin = aggragators.get("total_margin") or 0
+
+            try:
+                margin_percent = round(total_margin/total_omzet * 100, 2)
+            except:
+                margin_percent = 0
+
+            return total_omzet, total_qty, total_margin, margin_percent
+
+        items = []
+        for customer in queryset:
+            total_omzet, total_qty, total_margin, margin_percent = get_item(
+                customer)
+            items.append({
+                "customer": customer.name,
+                "omzet": total_omzet,
+                "qty": total_qty,
+                "margin": total_margin,
+                "margin_percent": margin_percent
+            })
+
+        output = create_xlsx_file(headers, items, True)
+        output.seek(0)
+        filename = (
+            f"customer_{datetime.datetime.now().strftime('%Y-%m-%d')}.xlsx"
+        )
+        http_response = HttpResponse(
+            output,
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        )
+        http_response["Content-Disposition"] = "attachment; filename=%s" % filename
+        http_response["Access-Control-Expose-Headers"] = "Content-Disposition"
+
+        return http_response
--- sales/views/sales.py
+++ sales/views/sales.py
@@ -1,25 +1,29 @@
+import datetime
 from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.utils.translation import gettext_lazy as _
 from django_filters import rest_framework as django_filters
+from django.db.models import Sum, F
+from django.shortcuts import HttpResponse
 from libs.filter import CreatedAtFilterMixin
 from libs.pagination import CustomPagination
+from libs.excel import create_xlsx_file
 from common.serializers import FileSerializer
 from common.models import File
 from ..scripts import generate_invoice_pdf_for_instances
-from ..serializers.sales import (SalesOrderSerializer, SalesOrderListSerializer, 
-SalesOrderDetailSerializer, InvoiceSerializer, SalesPaymentSerializer, SalesPaymentPartialUpdateSerializer)
-from ..models import SalesOrder, Invoice, SalesPayment, CustomerVisit
-
+from ..serializers.sales import (SalesOrderSerializer, SalesOrderListSerializer,
+                                 SalesOrderDetailSerializer, InvoiceSerializer, SalesPaymentSerializer, SalesPaymentPartialUpdateSerializer)
+from ..models import SalesOrder, Invoice, SalesPayment, CustomerVisit, OrderItem
 
 
 class SalesFilter(CreatedAtFilterMixin):
     status = django_filters.MultipleChoiceFilter(choices=SalesOrder.STATUS_CHOICES, help_text=_(
         'To filter multiple status, use this request example: ?status=requested&status=delivered'))
     id32s = django_filters.CharFilter(method='filter_by_id32s')
-    is_paid = django_filters.BooleanFilter(help_text=_('`true` for paid order, `false` for unpaid order'))
+    is_paid = django_filters.BooleanFilter(help_text=_(
+        '`true` for paid order, `false` for unpaid order'))
     trip_id32s = django_filters.CharFilter(method='filter_by_trip_id32s')
 
     class Meta:
@@ -34,8 +38,10 @@ class SalesFilter(CreatedAtFilterMixin):
     def filter_by_trip_id32s(self, queryset, name, value):
         # Split the comma-separated string to get the list of values
         values_list = value.split(',')
-        visits = CustomerVisit.objects.filter(trip__id32__in=values_list, sales_order__isnull=False)
+        visits = CustomerVisit.objects.filter(
+            trip__id32__in=values_list, sales_order__isnull=False)
         return queryset.filter(id__in=visits.values_list('sales_order', flat=True)).order_by('created_at')
+
 
 class SalesOrderViewSet(viewsets.ModelViewSet):
     lookup_field = 'id32'
@@ -75,6 +81,68 @@ class SalesOrderViewSet(viewsets.ModelViewSet):
         if not file:
             file = generate_invoice_pdf_for_instances(queryset)
         return Response(FileSerializer(instance=file).data)
+
+    @action(methods=["GET"], detail=False, url_path="excel")
+    def export_excel(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        headers = {
+            "order_date": "Order Date",
+            "invoice_number": "No. Nota",
+            "total_amount": "Jumlah",
+            "qty": "Quantity",
+            "total_margin_amount": "Margin",
+            "bonus": "Bonus",
+            "customer": "Customer",
+            "margin_percent": "% Margin",
+        }
+
+        def get_item(so):
+            current_order_items = so.order_items.filter(order=so)
+            aggragators = current_order_items.aggregate(total_amount=Sum(F('price') * F('quantity')), total_qty=Sum('quantity'),
+                                                        total_margin=F('total_amount') - Sum(F('product__base_price') * F('quantity')))
+            total_amount = aggragators.get("total_amount") or 0
+            total_qty = aggragators.get("total_qty") or 0
+            total_margin = aggragators.get("total_margin") or 0
+
+            try:
+                margin_percent = round(total_margin/total_amount * 100, 2)
+            except:
+                margin_percent = 0
+
+            return total_amount, total_qty, total_margin, margin_percent
+
+        items = []
+        for so in queryset:
+            total_amount, total_qty, total_margin, margin_percent = get_item(
+                so)
+            invoice_number = so.invoice.id32 if hasattr(
+                so, "invoice") else None
+            items.append({
+                "order_date": so.order_date,
+                "invoice_number": invoice_number,
+                "total_amount": total_amount,
+                "qty": total_qty,
+                "total_margin_amount": total_margin,
+                "bonus": so.bonus,
+                "customer": so.customer.name,
+                "margin_percent": margin_percent
+            })
+
+        output = create_xlsx_file(headers, items, True)
+        output.seek(0)
+        filename = (
+            f"salesorder_{datetime.datetime.now().strftime('%Y-%m-%d')}.xlsx"
+        )
+        http_response = HttpResponse(
+            output,
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        )
+        http_response["Content-Disposition"] = "attachment; filename=%s" % filename
+        http_response["Access-Control-Expose-Headers"] = "Content-Disposition"
+
+        return http_response
+
 
 class SalesPaymentViewSet(viewsets.ModelViewSet):
     queryset = SalesPayment.objects.all().order_by('-created_at')
