8fade75ff71ea891b14fccd995c992cb0a95c4ec
Fizi Kurniawan <fizikurniawan@gmail.com>
2024-05-13 11:06:18 +0700

    add aggregate by seller/sales

--- sales/router.py
+++ sales/router.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from .views.customer import CustomerViewSet, StoreTypeViewSet
-from .views.sales import SalesOrderViewSet, SalesPaymentViewSet
+from .views.sales import SalesOrderViewSet, SalesPaymentViewSet, RecordingSalesViewSet
 from .views.trip import (
     TripTemplateViewSet,
     TripViewSet,
@@ -18,3 +18,5 @@ router.register('store_type', StoreTypeViewSet, basename='store_type')
 router.register('customer', CustomerViewSet, basename='customer')
 router.register('sales_order', SalesOrderViewSet, basename='sales_order')
 router.register('sales_payment', SalesPaymentViewSet, basename='sales_payment')
+router.register('recording_sales', RecordingSalesViewSet,
+                basename='recording_sales')
