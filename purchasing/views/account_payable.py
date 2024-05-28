379fe7cdb57be189b3c9533ae65b1b0c54ef330b
Fizi Kurniawan <fizikurniawan@gmail.com>
2024-05-28 21:44:40 +0700

    add filter by date and search by supplier/vendor name

--- purchasing/views/account_payable.py
+++ purchasing/views/account_payable.py
@@ -1,19 +1,41 @@
 import datetime
-from inventory.models import StockMovement
 from purchasing.models import PurchaseOrder
 from django.shortcuts import HttpResponse
+from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
+from rest_framework import filters
 from rest_framework.viewsets import mixins, GenericViewSet
-from rest_framework.response import Response
 from rest_framework.decorators import action
 from libs.pagination import CustomPagination
 from libs.excel import create_xlsx_file
+
 from ..serializers.accounts_payable import AccountsPayableSerializer
+
+from django_filters import rest_framework as django_filters
+
+
+class AccountsPayableFilter(django_filters.FilterSet):
+    order_date_range = django_filters.CharFilter(
+        method='filter_order_date_range', help_text=_('Put date range in this format: "start_date,end_date" [YYYY-MM-DD,YYYY-MM-DD]'))
+
+    def filter_order_date_range(self, queryset, name, value):
+        if value:
+            # Split the value on a comma to extract the start and end dates
+            dates = value.split(',')
+            if len(dates) == 2:
+                start_date, end_date = dates
+                return queryset.filter(order_date__date__gte=start_date, order_date__date__lte=end_date)
+        return queryset
 
 
 class AccountsPayableViewSet(GenericViewSet, mixins.ListModelMixin):
     pagination_class = CustomPagination
     queryset = PurchaseOrder.objects.filter()
     serializer_class = AccountsPayableSerializer
+    filterset_class = AccountsPayableFilter
+    filter_backends = (filters.SearchFilter,
+                       django_filters.DjangoFilterBackend)
+    search_fields = ["supplier__name"]
 
     @action(methods=["GET"], detail=False, url_path="excel")
     def export_excel(self, request):
